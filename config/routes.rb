7f40422d0d70c4ecbac016b575ec39ea3c388e2e
BlackNesta <andreimanaila99@gmail.com>
2025-10-23 10:24:31 +0300

    add sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
+require "sidekiq/web"
+
 Rails.application.routes.draw do
+  # authenticate :user, lambda { |u| u.admin? } do
+    mount Sidekiq::Web => "/sidekiq"
+  # end
+
   devise_for :users, path: '', path_names: {
     sign_in: 'login',
     sign_out: 'logout',
